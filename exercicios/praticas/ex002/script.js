df4ef1289deb3674dbf0dd99d8522f2d06645ec2
JoanaBezerr4 <160871524+JoanaBezerr4@users.noreply.github.com>
2024-03-22 18:07:55 -0300

    .

--- exercicios/praticas/ex002/script.js
+++ exercicios/praticas/ex002/script.js
@@ -1,9 +1,34 @@
 function contar(){
-    var ini = document.getElementById('inicio')
+    var ini = document.getElementById('iinicio')
     var fim = document.getElementById('ifim')
     var passo = document.getElementById('ipasso')
     var resp = document.getElementById('res')
-    for (var c = ini; c <= fim; c += passo){
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length ==0){
+        window.alert('ERRO! Faltam números..')
+    } else{
+        resp.innerHTML= 'Contando...'
+        var i = Number(ini.value)
+        var f = Number(fim.value)
+        var p = Number(passo.value)
+        if (p <= 0){
+            window.alert('Impossível contar, considerando passo = 1')
+            p = 1
+        }
+        if (i < f){
+            for (var c = i; c <= f; c += p){
+                resp.innerHTML += ` => ${c}  `
+            }
+        } else{
+            for(var c = i; c >=f; c-= p){
+                resp.innerHTML += ` => ${c}  `
+            }
+        }
+        
+    }
+
+
+
+    /*for (var c = ini; c <= fim; c += passo){
         resp.innerHTML =`${c}`
-}
+    }*/
 }
